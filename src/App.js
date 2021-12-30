bd1dfb1162c1c1a9cb51dc0b65b8a2574ddd7cf9
Ndayisenga <cloude056@yahoo.fr>
2021-12-30 02:06:55 +0200

    NFT CRYPTO CODING

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import logo from './logo.svg';  
 import './App.css'; 
+import CollectionCard from './components/CollectionCard';
 import Header from './components/Header';
 
 function App() {
   return (
     <div className='app'>
       <Header />
+      <CollectionCard 
+      id={0} 
+      name={'Bandana Punk'} 
+      traits={[{'value': 7}]} 
+      image='https://ipfs.thirdweb.com/ipfs/bafybeigqkficum3anns36jid3dxvc4yfauyuvqjulbg43n23qxn3ce3tyu' 
+      />
     </div>
   )
 }
